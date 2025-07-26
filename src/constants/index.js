51acdbc53fc6a747812907c1c9f8dc6700e8e1fa
Hawzz7 <vicky.007.cr@gmail.com>
2025-07-26 16:11:17 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,247 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Amazon Clone",
+    description:
+      "A full-featured e-commerce web application that replicates core Amazon functionalities, enabling users to browse products, manage carts, and securely complete purchases.",
+    subDescription: [
+      "Developed a scalable MERN stack application with real-time cart and order management features.",
+      "Integrated Redux Toolkit and Redux Persist for efficient state management and data persistence across sessions.",
+      "Built a responsive and modern user interface using Tailwind CSS and Material UI (MUI).",
+      "Implemented secure user authentication, product filtering and search functionality.",
+      "Connected the backend with MongoDB for dynamic product, order, and user data handling.",
+    ],
+    href: "https://amzone-frontend.vercel.app/",
+    logo: "", // Add logo path if needed
+    image: "/assets/projects/amazon-clone.png", // Update with your project image path
+    tags: [
+      {
+        id: 1,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/expressjs.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 5,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 6,
+        name: "MUI",
+        path: "/assets/logos/mui.svg",
+      },
+      {
+        id: 7,
+        name: "Redux Toolkit",
+        path: "/assets/logos/redux.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Real-Time Chat Application",
+    description:
+      "A secure and responsive chat platform enabling real-time messaging, live status updates, and instant communication between users with end-to-end encryption.",
+    subDescription: [
+      "Engineered a real-time chat application using React.js, Node.js, Express.js, MongoDB, and Socket.io for seamless two-way messaging.",
+      "Implemented JWT and Bcrypt for secure authentication, reducing unauthorized login attempts across protected routes.",
+      "Enhanced user experience with live message delivery, online status indicators, and timestamped conversations.",
+      "Crafted a mobile-friendly interface using Tailwind CSS and DaisyUI for intuitive and accessible design.",
+      "Utilized Zustand for lightweight state management, ensuring smooth performance and component-level control.",
+    ],
+    href: "https://real-chat-app-vqwr.onrender.com/", // Add your GitHub or live project URL here
+    logo: "", // Add logo path if available
+    image: "/assets/projects/chat-app.png", // Update with your chat app image
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 3,
+        name: "Express.js",
+        path: "/assets/logos/expressjs.svg",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 5,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.svg",
+      },
+
+      {
+        id: 6,
+        name: "Socket.io",
+        path: "/assets/logos/socketio.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "BookMySeat",
+    description:
+      "A simple and intuitive movie seat booking app that allows users to browse movies, select showtimes, and book seats in real-time with a seamless user experience.",
+    subDescription: [
+      "Developed a movie ticket booking platform using MongoDB, Express.js, React.js, and Node.js (MERN stack) for real-time ticket reservations.",
+      "Enabled over 90% booking accuracy by designing a smooth task flow with minimal user interaction friction.",
+      "Crafted a responsive and interactive UI using React.js, Tailwind CSS, and Material-UI, improving seat selection efficiency by 85%.",
+      "Integrated backend services using Node.js, Express.js, and MongoDB to ensure 100% data reliability for bookings and user operations.",
+      "Optimized frontend-backend interactions to deliver a fast, real-time experience with clean UI feedback.",
+    ],
+    href: "https://appbookmyshow.netlify.app/",
+    image: "/assets/projects/bookmyseat.png",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.svg",
+      },
+      {
+        id: 2,
+        name: "Express.js",
+        path: "/assets/logos/express.svg",
+      },
+      {
+        id: 3,
+        name: "React.js",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/logos/nodejs.svg",
+      },
+      {
+        id: 5,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 6,
+        name: "Material UI",
+        path: "/assets/logos/mui.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/917869269847",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/nkm-narenda-786700cr/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/vicky.007.cr/",
+    icon: "/assets/socials/instagram.svg",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/Hawzz7",
+    icon: "/assets/logos/github3.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Teaching Assistant Intern",
+    job: "AlmaBetter",
+    date: "Aug 2024 – Sept 2024",
+    contents: [
+      "Mentored 10–15 students daily, resolving project-related queries in full-stack technologies including React.js, Express.js, and MongoDB.",
+      "Reviewed student code and debugged issues, reducing project errors by 40% and promoting clean, efficient practices.",
+      "Collaborated on 10+ learning resources with instructors, increasing assignment completion by 25% among students.",
+      "Guided learners in building full-stack projects, improving project quality by 80% through clear frontend, backend, and database structure.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2024-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
---
+++ src/sections/About.jsx
@@ -0,0 +1,118 @@
+import { useRef } from "react";
+import Card from "../components/Card";
+import { Globe } from "../components/Globe";
+import CopyEmailButton from "../components/CopyEmailButton";
+import { Frameworks } from "../components/Frameworks";
+
+const About = () => {
+  const grid2Container = useRef();
+  return (
+    <section className="c-space section-spacing">
+      <h2 className="text-heading">About Me</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
+        {/* grid-1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Narendra Kumar Majhi</p>
+            <p className="subtext">
+              I have developed strong frontend and backend development skills
+              through building real-world projects, enabling me to create
+              dynamic, responsive, and user-centric web applications.
+            </p>
+          </div>
+          <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
+        </div>
+        {/* grid-2 */}
+        <div className="grid-default-color grid-2">
+          <div
+            ref={grid2Container}
+            className="flex items-center justify-center w-full h-full"
+          >
+            <p className="flex items-end text-5xl text-gray-500">
+              CODE IS CRAFT
+            </p>
+            <Card
+              style={{ rotate: "20deg", top: "30%", left: "20%" }}
+              image="assets/logos/css3.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+              image="assets/logos/expressjs.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-40deg", bottom: "30%", left: "70%" }}
+              image="assets/logos/mongodb.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+              image="assets/logos/html5.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "20deg", top: "10%", left: "38%" }}
+              image="assets/logos/javascript.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "30deg", top: "70%", left: "70%" }}
+              image="assets/logos/react.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+              image="assets/logos/tailwindcss.svg"
+              containerRef={grid2Container}
+            />
+            <Card
+              style={{ rotate: "0deg", top: "5%", left: "10%" }}
+              image="assets/logos/git.svg"
+              containerRef={grid2Container}
+            />
+          </div>
+        </div>
+        {/* grid-3 */}
+        <div className="grid-black-color grid-3">
+          <div className="z-10 w-[50%]">
+            <p className="headtext">Time Zone</p>
+            <p className="subtext">
+              I'm based in India, and open to work worldwide
+            </p>
+          </div>
+          <figure className="absolute left-[30%] top-[10%]">
+            {/* <Globe /> */}
+          </figure>
+        </div>
+        {/* grid-4 */}
+        <div className="grid-special-color grid-4">
+          <div className="flex flex-col items-center justify-center gap-4 size-full">
+            <p className="text-center headtext">
+              Do you want to start a project together?
+            </p>
+            <CopyEmailButton />
+          </div>
+        </div>
+        {/* grid-5 */}
+        <div className="grid-default-color grid-5">
+          <div className="z-10 w-[50%]">
+            <p className="headtext">Teck Stack</p>
+            <p className="subtext">
+              I work with a range of modern languages, frameworks, and tools to build reliable, scalable, and user-focused web applications.
+            </p>
+          </div>
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale">
+            <Frameworks />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
